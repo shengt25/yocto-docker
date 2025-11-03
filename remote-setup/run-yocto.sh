ff14148e0ab0f2e78b23f9a000d2182cb5f30176
Sheng Tai <shengtai25@outlook.com>
2025-11-03 17:30:18 +0200

    cleanup unused files

--- remote-setup/run-yocto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker compose -f compose.yocto.yaml up -d
-CONTAINER_NAME=$(docker compose -f compose.yocto.yaml ps -q yocto)
-docker exec -it $CONTAINER_NAME /bin/zsh
-docker compose -f compose.yocto.yaml down
