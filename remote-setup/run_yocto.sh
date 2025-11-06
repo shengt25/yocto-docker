f026c3c0e4b8213e7515a5131ff43b9f6cc22b58
shengt25 <shengtai25@outlook.com>
2025-11-06 18:44:52 +0200

    Add error handling for yocto container startup failure

--- remote-setup/run_yocto.sh
+++ remote-setup/run_yocto.sh
@@ -2,5 +2,11 @@
 
 docker compose -f compose.yocto.yaml up -d
 CONTAINER_NAME=$(docker compose -f compose.yocto.yaml ps -q yocto)
+if [ -z "$CONTAINER_NAME" ]; then
+    echo "Failed to get yocto container ID."
+    echo "Maybe the container failed to start?"
+    echo "Check with docker compose -f compose.yocto.yaml ps" 
+    exit 1
+fi
 docker exec -it $CONTAINER_NAME /bin/zsh
 docker compose -f compose.yocto.yaml stop
